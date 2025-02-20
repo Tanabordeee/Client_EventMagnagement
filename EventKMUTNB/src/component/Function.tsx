0fa6d24246b6ac397d8457880716985689307471
Athichanun <waveathichanun@gmail.com>
2025-02-21 01:49:21 +0700

    missing login to home commit

--- EventKMUTNB/src/component/Function.tsx
+++ EventKMUTNB/src/component/Function.tsx
@@ -4,8 +4,8 @@ function Function() {
     <div className="fucntion">
         <div className="flex p-2">
             <p className='p-2'>history</p>
-            <p className='p-2'><Link to = '/notification'>Notification</Link></p>
-            <p className='p-2'><Link to = '/setting'>Profile</Link></p>
+            <p className='p-2'><Link to = "notification">Notification</Link></p>
+            <p className='p-2'><Link to = "setting">Profile</Link></p>
         </div>
     </div>
   )
