1e1b92ef2935cea9b85e3f900ab2820e0ec47843
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-16 00:23:30 +0700

    fix footer

--- src/component/user/Menu.tsx
+++ src/component/user/Menu.tsx
@@ -55,7 +55,7 @@ function Menu() {
               ))}
             </div>
           </div>
-          <footer className='footer'>
+          <footer className='footer p-5'>
             <p>Contact us</p>
             <p>tell : 099-999-9999</p>
             <p>line : @gm2</p>
