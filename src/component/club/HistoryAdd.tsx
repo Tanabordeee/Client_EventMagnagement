37446afdfd732ae2dfdce90f554b84e0a2337e39
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-29 12:14:21 +0700

    add index

--- src/component/club/HistoryAdd.tsx
+++ src/component/club/HistoryAdd.tsx
@@ -39,10 +39,10 @@ function HistoryAdd() {
         <div className="m-4">
             <div className="flex text-3xl font-bold p-4 pt-10">History</div>
             <div className="flex flex-wrap p-3">
-                {histevent.map(value => {
+                {histevent.map((value , index) => {
                     return(
                         <div className="2xl:w-1/3 w-1/2 max-lg:w-1/1 flex justify-center items-center pt-10">
-                            <div className=" mx-3 my-2 rounded-xl"><EventClub Event = {value}/></div>
+                            <div className=" mx-3 my-2 rounded-xl flex flex-col justify-center items-center"><h1 className="text-2xl font-bold">Event {index + 1}</h1><EventClub Event = {value}/></div>
                         </div>
                     )
                 })}
