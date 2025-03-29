66e4d27aadbb9385718c7ecc7dba6e94b2f397b3
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-29 13:09:14 +0700

    remove faculty and major

--- src/component/club/List.tsx
+++ src/component/club/List.tsx
@@ -40,12 +40,10 @@ function List() {
         <div className="p-4 ">
             <div className ="flex-col overflow-x-auto">
             <div className="flex justify-center p-3 text-2xl max-lg:text-md font-bold pb-7">จำนวนนักศึกษาที่พบ : {(histevent ?? []).reduce((acc, value) => acc + (value.users ? value.users.length : 0), 0)}</div>
-                <div className="grid grid-cols-6">
+                <div className="grid grid-cols-4">
                     <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">Time</div>
                     <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">Title</div>
-                    <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">Faculty</div>
                     <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">Name</div>
-                    <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">Major</div>
                     <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">ID</div>
                 </div>
                     {(histevent).map((value, index) => {
@@ -54,14 +52,12 @@ function List() {
                                 {(value.users).map((users, userindex) => {
                                     // setStudent(student + 1);
                                     return(
-                                        <div className="grid grid-cols-6" key={userindex}>
+                                        <div className="grid grid-cols-4" key={userindex}>
                                             <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">
                                                 {new Date(value.eventDate).toISOString().split('T')[0]}
                                             </div>
                                             <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">{value.eventName}</div>
-                                            <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">-</div>
                                             <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">{users.username}</div>
-                                            <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">-</div>
                                             <div className=" border w-auto break-words text-center text-xl max-lg:text-sm">{users.userId}</div>
                                         </div>
                                         
