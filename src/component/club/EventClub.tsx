c53997d3b4f5edd95709f8e98fbfc9aa1670d9f5
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-29 15:31:43 +0700

    remove view

--- src/component/club/EventClub.tsx
+++ src/component/club/EventClub.tsx
@@ -1,5 +1,4 @@
 import dayjs from 'dayjs';
-import React, { useState } from 'react'
 
 interface event {
     Event :{
@@ -13,17 +12,13 @@ interface event {
 }
 
 const  EventClub : React.FC<event> = ({Event}) => {
-    const [isclick, setIsclick] = useState(false);
-    const setclick = () => {
-        setIsclick(!isclick);
-    }
   return (
-    <div className={`grid p-2 justify-center m-3 bg-[#E7E9EC] rounded-xl shadow-lg ${isclick? 'transition-transform transform scale-125' : ''}`}>
+    <div className={`grid p-2 justify-center m-3 bg-[#E7E9EC] rounded-xl shadow-lg`}>
         <div className="flex justify-between w-100 px-3 max-md:w-60 relative">
             <div className="flex-col  py-2">
                 <img src={Event.image} className='w-50 h-40 max-md:w-40 max-md:20 rounded-xl object-cover' />
                 <div className= {`p-2 flex justify-center gap-2 bg-white rounded-lg m-3`}>
-                    สถานะ : <div className={`${Event.status == 'not approve'? 'text-red-500' : 'text-green-500'}`}>{Event.status == 'not approve'? 'ยังไม่อนุมัติ' : 'อนุมัติแล้ว'}</div> 
+                    สถานะ : <div className={` ${Event.status == 'not approve'? 'text-red-500' : 'text-green-500'}`}>{Event.status == 'not approve'? 'ยังไม่อนุมัติ' : 'อนุมัติแล้ว'}</div> 
                 </div>
             </div>
             <div className="flex-col py-2 px-2 ">
@@ -32,12 +27,6 @@ const  EventClub : React.FC<event> = ({Event}) => {
                 <p className='flex justify-center gap-2'>วันที่ : <p className='flex justify-center'>{dayjs(Event.eventDate).format("YYYY-MM-DD")}</p></p>
                 <p className='flex justify-center gap-2'>เวลา : <p className='flex justify-center'>{Event.time}</p></p>
                 <p className='flex justify-center gap-2'>ติดต่อ : <a className='text-blue-500' href="https://www.kmutnb.ac.th/" target="_blank" rel="noopener noreferrer">KMUTNB</a></p>
-                <div className="flex justify-end">
-                    <button className="hover:cursor-pointer px-2 rounded-xl text-xl absolute bottom-0"
-                        onClick={setclick}>
-                            View
-                    </button>
-                </div>
             </div>
         </div>
     </div>
