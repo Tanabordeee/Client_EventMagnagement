8d025a2e8a69e4078505360ed103a70604094cda
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-29 15:56:40 +0700

    handle error fire

--- src/component/club/HistoryAdd.tsx
+++ src/component/club/HistoryAdd.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import EventClub from "./EventClub"
 import axios from "axios";
 import { useOutletContext } from "react-router-dom";
+import Swal from "sweetalert2";
 
 interface Event{
     eventName : string;
@@ -23,7 +24,17 @@ function HistoryAdd() {
             const respon = await axios.get(url, {withCredentials : true});
             if(search){
                 const searcher = respon.data.filter((item :Event) => item.eventName === search)
-                setHistevent(searcher);
+                if(searcher.length > 0){
+                    setHistevent(searcher);
+                }else{
+                    Swal.fire({
+                        title : 'ไม่พบข้อมูล',
+                        text : 'ไม่พบข้อมูลกิจกรรมที่ค้นหา', 
+                        icon : 'error',
+                        confirmButtonText : 'ตกลง'
+                    })
+                    setHistevent(respon.data);
+                }
             }else{
                 setHistevent(respon.data);
             }
@@ -42,7 +53,7 @@ function HistoryAdd() {
                 {histevent.map((value , index) => {
                     return(
                         <div className="2xl:w-1/3 w-1/2 max-lg:w-1/1 flex justify-center items-center pt-10">
-                            <div className=" mx-3 my-2 rounded-xl flex flex-col justify-center items-center"><h1 className="text-2xl font-bold">Event {index + 1}</h1><EventClub Event = {value}/></div>
+                            <div className=" mx-3 my-2 rounded-xl flex flex-col justify-center items-center"><h1 className="text-2xl font-bold">Concert {index + 1}</h1><EventClub Event = {value}/></div>
                         </div>
                     )
                 })}
